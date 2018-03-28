40881f8107627efa305cddc97de1982da2d7e86d
Ramana48 <sreeramana48@gmail.com>
2018-03-28 23:42:23 +0530

    Update Lottery contract

--- Lottery.sol
+++ Lottery.sol
@@ -1,41 +1,74 @@
 pragma solidity ^0.4.11;
 
-contract Lottery {
+contract Ownable {
+  
+  address public owner;
 
-    mapping(address => uint) usersBet;
+  event OwnershipTransferred(address indexed_previousOwner, address indexed_newOwner);
+
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+ // Modifier for Checking Ownership 
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+ //Changing Ownership
+  function transferOwnership(address newOwner) onlyOwner public {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+}
+contract Lottery is Ownable{
+
+    mapping(address => uint) usersLottery;
     mapping(uint => address) users;
+    
     uint nbUsers = 0;
     uint totalBets = 0;
-
-    address owner;
-
-    function Lottery() {
-        owner = msg.sender;
-    }
-    
+    address winnerAddress;
+    event WinnerAddress(address winner);
+   
     function Buy() public payable  {
         if (msg.value > 0) {
-            if (usersBet[msg.sender] == 0) {
+            
+            if (usersLottery[msg.sender] == 0) {
                 users[nbUsers] = msg.sender;
                 nbUsers += 1;
             }
-            usersBet[msg.sender] += msg.value;
+            
+            usersLottery[msg.sender] += msg.value;
             totalBets += msg.value;
         }
     }
     
-    function PicLottery() public {
-        if (msg.sender == owner) {
+    function PicLottery() public  onlyOwner constant returns(address winnerAddress){
+        
+            // Minimum 3 users required to PicLottery
+            if(nbUsers>=3)       
+            {
+                
             uint sum = 0;
+            
+            // Generating Random number 
             uint winningNumber = uint(block.blockhash(block.number-1)) % totalBets + 1;
-            for (uint i=0; i < nbUsers; i++) {
-                sum += usersBet[users[i]];
-                if (sum >= winningNumber) {
-                    selfdestruct(users[i]);
-                    return;
+            for (uint i=0; i < nbUsers; i++) 
+              {
+                sum += usersLottery[users[i]];
+                if (sum >= winningNumber) 
+                {
+                    // Transfering funds to winner
+                    users[i].transfer(totalBets);
+                    winnerAddress=users[i];
+                    WinnerAddress(users[i]);
+                    
+                    return users[i];
                 }
+              }
             }
         }
-    }
-    
 }
